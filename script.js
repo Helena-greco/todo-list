5c7c800b24c692e47a4666fd6e8871e355094f79
Helena <nenafgreco@hotmail.com>
2021-07-09 19:59:57 -0300

    Refatorando o codigo e cumprindo o requisito 8

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const buttonAddList = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const inputText = document.querySelector('#texto-tarefa');
+const clearButton = document.querySelector('#apaga-tudo');
+const clearSelected = document.querySelector('#remover-selecionado');
 
-function selectLi(li) {
-  li.addEventListener('click', () => {
-    li.classList.remove('selected');
-    li.classList.add('selected');
-  });
-}
-
-buttonAddList.addEventListener('click', () => {
+function addList() {
   const li = document.createElement('li');
   li.innerHTML = inputText.value;
   taskList.appendChild(li);
+  li.classList.add('toDoList');
   inputText.value = '';
-  selectLi(li);
-});
+}
+
+function changeBGColor(li) {
+  const lis = document.getElementsByClassName('toDoList');
+  for (let index = 0; index < lis.length; index += 1) {
+    lis[index].style.backgroundColor = '';
+  }
+  li.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+buttonAddList.addEventListener('click', addList);
+
+taskList.addEventListener('click', changeBGColor);
